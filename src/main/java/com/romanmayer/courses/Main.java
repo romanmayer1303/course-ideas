a42a0d1740d63b0bd96982782748921b1808614e
romanmayer <romanmayer1303@gmail.com>
2017-01-02 11:57:56 +0100

    Initial commit

---
+++ src/main/java/com/romanmayer/courses/Main.java
@@ -0,0 +1,33 @@
+package com.romanmayer.courses;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+/**
+ * Created by romanmayer on 01/01/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+}
---
+++ src/main/java/com/romanmayer/courses/model/CourseIdea.java
@@ -0,0 +1,11 @@
+package com.romanmayer.courses.model;
+
+/**
+ * Created by romanmayer on 02/01/17.
+ */
+public class CourseIdea {
+
+    private String title;
+    private String creator;
+
+}
